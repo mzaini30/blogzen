687cf7e49b40f4f9166da456b2e5c8aa72f00f3f
mzaini30 <muhzaini30@gmail.com>
2020-09-14 00:12:51 +0800

    upload

--- src/halaman/Tulis.svelte
+++ src/halaman/Tulis.svelte
@@ -3,7 +3,7 @@
 		<input type="search" bind:this={ref} bind:value={judul} class="form-control" required placeholder="Judul" name="">
 	</div>
 	<div class="form-group">
-		<textarea style="height: {tinggi}px" bind:value={isi} class="form-control" placeholder="Isi. Menggunakan Markdown"></textarea>
+		<textarea bind:this={tinggi_textarea} bind:value={isi} class="form-control" placeholder="Isi. Menggunakan Markdown"></textarea>
 	</div>
 	<div class="form-group">
 		<input type="submit" class="btn btn-success float-right" value="&#x2714;" name="">
@@ -13,8 +13,8 @@
 <script type="text/javascript">
 	import {onMount} from 'svelte'
 	import {push} from 'svelte-spa-router'
-	let tinggi = window.innerHeight - 200
 	let ref
+	let tinggi_textarea
 	let judul = ''
 	let isi = ''
 	const submit = () => {
@@ -26,5 +26,10 @@
 			body
 		}).then(x => x.json()).then(y => push(`/${y}`))
 	}
-	onMount(() => ref.focus())
+	onMount(() => {
+		ref.focus()
+		const ukur_tinggi = () => tinggi_textarea.style.height = `${window.innerHeight - 200}px`
+		window.addEventListener('resize', ukur_tinggi)
+		ukur_tinggi()
+	})
 </script>
