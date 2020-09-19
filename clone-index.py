1894342fe2c6cfea2cdf156d5b6ad279ba19f0cd
mzaini30 <muhzaini30@gmail.com>
2020-09-19 19:39:25 +0800

    upload

--- clone-index.py
+++ clone-index.py
@@ -1,4 +1,5 @@
 import os, json
+
 os.chdir("public")
 with open("db.json") as f:
  data = json.load(f)
@@ -8,8 +9,3 @@ for x in data["postingan"]:
 for x in slug:
  os.system('cp index.html ' + x + '.html')
 os.system("cp index.html 404.html")
-os.chdir("..")
-os.system("git status")
-os.system("git add -A .")
-os.system('git commit -m "upload"')
-os.system("git push")
