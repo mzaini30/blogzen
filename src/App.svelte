0e7817e5d64ee64562a699fb86c30263b00714de
mzaini30 <muhzaini30@gmail.com>
2020-09-19 09:28:20 +0800

    upload

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+ import {Link, Router, Route} from "svelte-routing"
+ import Beranda from "./halaman/Beranda.svelte"
+ export let url = ""
+</script>
+
+<Router {url}>
+ <div>
+  <Route path="/"><Beranda/></Route>
+ </div>
+</Router>
---
+++ src/halaman/Beranda.svelte
@@ -0,0 +1,1 @@
+<p>Ini Beranda</p>
