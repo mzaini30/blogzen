b3a338177bce5042037721382b6fb70f751cd908
mzaini30 <muhzaini30@gmail.com>
2020-09-27 18:58:35 +0800

    upload

--- server.go
+++ server.go
@@ -1,11 +1,8 @@
 package main
 import (
 	"net/http"
-	"fmt"
 	spa "github.com/roberthodgen/spa-server"
 )
 func main(){
 	http.ListenAndServe(":2020", spa.SpaHandler("public", "index.html"))
-	fmt.Println("Blog Zen aktif! Yey....")
-	fmt.Println("Buka localhost:2020")
 }
