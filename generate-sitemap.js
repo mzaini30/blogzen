a7eb865fbf1e383825913c3d2daae842791d2687
mzaini30 <muhzaini30@gmail.com>
2020-10-01 22:26:31 +0800

    upload

---
+++ generate-sitemap.js
@@ -0,0 +1,23 @@
+process = require('process')
+fs = require('fs')
+util = require('util')
+readFile = util.promisify(fs.readFile)
+writeFile = util.promisify(fs.writeFile)
+process.chdir('public')
+baca = async () => {
+	data = await readFile('db.json')
+	data = await JSON.parse(data)
+	slug = []
+	for (x of data.postingan){
+		slug.push(x.slug)
+	}
+	slug.reverse()
+	sitemap = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">'
+	for (x of slug){
+		sitemap += `<url><loc>https://blogzen.js.org/${x}</loc></url>`
+	}
+	sitemap += '<url><loc>https://blogzen.js.org/</loc></url>'
+	sitemap += '</urlset>'
+	writeFile('sitemap.xml', sitemap)
+}
+baca()
