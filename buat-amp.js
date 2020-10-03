d11dda29e2122a8405bd339358f61523221d58a2
mzaini30 <muhzaini30@gmail.com>
2020-10-03 22:10:27 +0800

    upload

--- buat-amp.js
+++ buat-amp.js
@@ -9,6 +9,7 @@ marked.setOptions({
 })
 process.chdir("public")
 jalankan = async () => {
+ css = await readFile("water.min.css", "utf8")
 	baca = await readFile("db.json", "utf8")
 	baca = JSON.parse(baca)
 	process.chdir("amp")
@@ -47,6 +48,7 @@ jalankan = async () => {
       }
     </script>
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+    <style amp-custom>${css}</style>
   </head>
   <body>
     <h1>${x.judul}</h1>
