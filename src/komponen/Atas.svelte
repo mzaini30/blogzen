0734c4ef537663d04651a9279d58afe10f9d6097
mzaini30 <muhzaini30@gmail.com>
2022-11-18 11:58:12 +0800

    Termux

--- src/komponen/Atas.svelte
+++
@@ -1,8 +0,0 @@
-<div class="btn btn-light" on:click={atas}>&uarr;</div>
-<script type="text/javascript">
-	const atas = () => window.scrollTo({
-		top: 1,
-		left: 0,
-		behavior: 'smooth'
-	})
-</script>
--- start.sh
+++
@@ -1,2 +0,0 @@
-cd public 
-php -S localhost:2020 & json-server --watch db.json
